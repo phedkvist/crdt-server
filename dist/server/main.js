0ee9b70b5b0c7d0fcd3653f48801b35a198bb574
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2019-04-10 20:13:47 +0200

    Store changes in-memory

--- dist/server/main.js
+++ dist/server/main.js
@@ -8,19 +8,21 @@ const app = express();
 const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
-var changes = [];
-wss.on('connection', function (ws) {
+let changes = new Array();
+wss.on('connection', (ws) => {
     //connection is up, let's add a simple simple event
-    ws.on('message', function (message) {
+    ws.on('message', (message) => {
         //log the received message and send it back to the client
-        //console.log('received: %s', message);
+        console.log('received: %s', message);
         var change = JSON.parse(message);
         if (change.type === 'request_init_load') {
-            ws.send(JSON.stringify({type: 'init_load', data: changes}))
-        } else {
+            ws.send(JSON.stringify({ type: 'init_load', data: changes }));
+            console.log('sent the messages');
+        }
+        else {
             if (['insert', 'delete', 'retain'].includes(change.type)) {
                 changes.push(message);
-                console.log(changes.length)
+                console.log(changes.length);
             }
             //send back the message to the other clients
             wss.clients
--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
+let changes = new Array<string>();
 
 wss.on('connection', (ws: WebSocket) => {
 
@@ -18,17 +19,27 @@ wss.on('connection', (ws: WebSocket) => {
         //log the received message and send it back to the client
         console.log('received: %s', message);
 
-        //send back the message to the other clients
-        wss.clients
-        .forEach(client => {
-            if (client != ws) {
-                client.send(message);
-            }    
-        });
+        var change = JSON.parse(message);
+        if (change.type === 'request_init_load') {
+            ws.send(JSON.stringify({type: 'init_load', data: changes}))
+            console.log('sent the messages');
+        } else {
+            if (['insert', 'delete', 'retain'].includes(change.type)) {
+                changes.push(message);
+                console.log(changes.length)
+            }
+            //send back the message to the other clients
+            wss.clients
+                .forEach(function (client) {
+                if (client != ws) {
+                    client.send(message);
+                }
+            });
+        }
     });
 });
 
 //start our server
-server.listen(process.env.PORT || 8999, () => {
+server.listen(process.env.PORT || 5000, () => {
     console.log(`Server started`);
 });
